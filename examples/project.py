8577dae04d324b292fbc65be899a6c142d3c8dc5
khrapovs <khrapovs@gmail.com>
2014-12-24 12:44:45 +0300

    Clean Sobol code

--- examples/project.py
+++ examples/project.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
-# from numpy import sqrt, sum
-#from random import normalvariate
+# -*- coding: utf-8 -*-
+"""Compare means and variances of uniform and Sobol random numbers.
+
+"""
 from math import *
+import numpy as np
 from scipy.stats import norm
-from sobol import *
+from sobol import i4_sobol_generate
 
 #y=normalvariate(0,1)
 
 #i4_sobol_generate(2,1000,3).transpose()
-f=i4_sobol_generate(2,1000,3).transpose()
+f = i4_sobol_generate(2, 1000, 3).transpose()
 #y=norm.ppf(self.u[self.f,0])
- 
+
 
 def MC(nn):
-    mean=0.0
-    var=0.0
-#        t1_avr=
-    for i1 in range(nn):          
-        u=f[i1][0]
-        y=norm.ppf(u)
-        mean+=y
-        var+=y*y        
-    return (mean/nn,var/nn)
+    mean, var = 0, 0
+    for i1 in range(nn):
+        u = f[i1][0]
+        y = norm.ppf(u)
+        mean += y
+        var += y*y
+    return (mean/nn, var/nn)
 
 n = 1000
-print MC(n)
+print(MC(n))
 
 R = norm.rvs(size=n)
 
-print (mean(R), var(R))
+print((np.mean(R), np.var(R)))
--- readme.rst
+++ readme.rst
@@ -2,3 +2,5 @@ Sobol pseudo-random number generator
 ====================================
 
 `Documentation <http://sobol.readthedocs.org/en/latest/>`_
+
+Original code: http://people.sc.fsu.edu/~jburkardt/py_src/sobol/sobol.html
